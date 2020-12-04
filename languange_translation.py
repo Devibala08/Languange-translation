45f1662734d96e6f9f50968566c379366674d778
Devibala K <65863830+Devibala08@users.noreply.github.com>
2020-12-04 00:30:48 -0800

    Add files via upload

---
+++ languange_translation.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+with open('t8.shakespeare.txt') as file:
+    data = file.read()
+
+words  = pd.read_excel('translate.xlsx')  # excel file
+
+orignal_text = data
+n= 0
+while n < len(words):
+    txt1 = str(words['English'].iloc[n])
+    translated_word = words['French'].iloc[n]
+    orignal_text = orignal_text.replace(txt1, translated_word)
+    #print(n)
+    n = n + 1
+print(orignal_text)
+file = open('Translated_text2.txt', 'w')
+file.write(orignal_text)
+file.close()
