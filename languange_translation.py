035266f958ade4b5850da21c5fa9ffcd638b4a9d
Devibala K <65863830+Devibala08@users.noreply.github.com>
2021-04-09 20:25:44 +0530

    Update languange_translation.py

--- languange_translation.py
+++ languange_translation.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import os, psutil
+import timeit
+setup_code = "from math import factorial"
 
 with open(r'C:\Users\welcome\Desktop\t8.shakespeare.txt') as file:
     data = file.read()
@@ -14,6 +17,10 @@ while n < len(words):
     #print(n)
     n = n + 1
 #print(orignal_text)
-file = open('Translated_text3.txt', 'w')
+file = open(r'C:\Users\welcome\Desktop\Translated_text4.xlsx','w')
 file.write(orignal_text)
 file.close()
+
+print(f"Execution time is: {timeit.timeit(setup = setup_code, number = 10000000)}")
+process = psutil.Process(os.getpid())
+print(f"Used memory is bytes: {psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2}")
